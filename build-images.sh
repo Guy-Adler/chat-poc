4b7c64e101b60c16b78f3b2cdf6c0468c97424cd
Guy Adler <guyadler2005@gmail.com>
2025-06-21 21:12:33 +0300

    Add build script for Docker images with parallel build and push functionality

---
+++ build-images.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -euo pipefail
+
+buildAndTag() {
+  local service=$1
+
+  if [[ -z "$service" ]]; then
+    echo 'Invalid argument; need to provide $service';
+    return 1
+  fi
+
+  echo "Starting to build $service..."
+  docker build --quiet "$service" -t "chat-$service" > /dev/null
+  echo "Finished building $service. Starting to push"
+  docker tag "chat-$service" "localhost:32000/chat-$service" > /dev/null
+  docker push "localhost:32000/chat-$service" > /dev/null
+  echo "Finished pushing $service"
+}
+
+
+services=()
+while IFS= read -r service; do
+  services+=("$service")
+done < <(find . -maxdepth 2 -name Dockerfile -type f -exec dirname {} \; | sed 's|^\./||')
+
+echo "Found the following services:"
+echo "$services"
+
+echo "Running build & push in parallel..."
+
+for service in "${services[@]}"; do
+  buildAndTag "$service" &
+done
+
+wait
+echo "Finished building & pushing all services"
